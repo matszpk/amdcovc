c70204c07b11bb2b58dff4f2f1699d92a26fed00
wilvk <willvk@gmail.com>
2017-11-05 10:59:36 +1100

    cleaning

--- amdcovc.cpp
+++ amdcovc.cpp
@@ -220,6 +220,7 @@ catch(...)
     if (handle != nullptr)
     {
         dlerror(); // clear old errors
+
         if (dlclose(handle)) // if closing failed
         {
             handle = nullptr;
@@ -235,6 +236,7 @@ ATIADLHandle::~ATIADLHandle()
     if (handle != nullptr)
     {
         dlerror();
+
         if (dlclose(handle))
         {
             handle = nullptr;
@@ -247,142 +249,179 @@ ATIADLHandle::~ATIADLHandle()
 void* ATIADLHandle::getSym(const char* symbolName)
 {
     void* symbol = nullptr;
+
     dlerror(); // clear old errors
     symbol = dlsym(handle, symbolName);
     const char* error = dlerror();
+
     if (symbol == nullptr && error != nullptr)
+    {
         throw Error(error);
+    }
+
     return symbol;
 }
 
-void ATIADLHandle::Main_Control_Create(ADL_MAIN_MALLOC_CALLBACK callback,
-                            int iEnumConnectedAdapters) const
+void ATIADLHandle::Main_Control_Create(ADL_MAIN_MALLOC_CALLBACK callback, int iEnumConnectedAdapters) const
 {
     int error = pADL_Main_Control_Create(callback, iEnumConnectedAdapters);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Main_Control_Create error");
+    }
 }
 
 void ATIADLHandle::Main_Control_Destroy() const
 {
     int error = pADL_Main_Control_Destroy();
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Main_Control_Destroy error");
+    }
 }
 
 void ATIADLHandle::ConsoleMode_FileDescriptor_Set(int fileDescriptor) const
 {
     int error = pADL_ConsoleMode_FileDescriptor_Set(fileDescriptor);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_ConsoleMode_FileDescriptor_Set error");
+    }
 }
 
 void ATIADLHandle::Adapter_NumberOfAdapters_Get(int* number) const
 {
     int error = pADL_Adapter_NumberOfAdapters_Get(number);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Adapter_NumberOfAdapters_Get error");
+    }
 }
 
 void ATIADLHandle::Adapter_Active_Get(int adapterIndex, int* status) const
 {
     int error = pADL_Adapter_Active_Get(adapterIndex, status);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Adapter_Active_Get error");
+    }
 }
 
 void ATIADLHandle::Adapter_Info_Get(LPAdapterInfo info, int inputSize) const
 {
     int error = pADL_Adapter_AdapterInfo_Get(info, inputSize);
+    
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_AdapterInfo_Get error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_CurrentActivity_Get(int adapterIndex,
-                ADLPMActivity* activity) const
+void ATIADLHandle::Overdrive5_CurrentActivity_Get(int adapterIndex, ADLPMActivity* activity) const
 {
     int error = pADL_Overdrive5_CurrentActivity_Get(adapterIndex, activity);
+
     if (error != ADL_OK)
         throw Error(error, "ADL_Overdrive5_CurrentActivity_Get error");
 }
 
-void ATIADLHandle::Overdrive5_Temperature_Get(int adapterIndex, int thermalCtrlIndex,
-                ADLTemperature *temperature) const
+void ATIADLHandle::Overdrive5_Temperature_Get(int adapterIndex, int thermalCtrlIndex, ADLTemperature *temperature) const
 {
-    int error = pADL_Overdrive5_Temperature_Get(adapterIndex, thermalCtrlIndex,
-                    temperature);
+    int error = pADL_Overdrive5_Temperature_Get(adapterIndex, thermalCtrlIndex, temperature);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_Temperature_Get error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_FanSpeedInfo_Get(int adapterIndex, int thermalCtrlIndex,
-                ADLFanSpeedInfo* fanSpeedInfo) const
+void ATIADLHandle::Overdrive5_FanSpeedInfo_Get(int adapterIndex, int thermalCtrlIndex, ADLFanSpeedInfo* fanSpeedInfo) const
 {
-    int error = pADL_Overdrive5_FanSpeedInfo_Get(adapterIndex, thermalCtrlIndex,
-                    fanSpeedInfo);
+    int error = pADL_Overdrive5_FanSpeedInfo_Get(adapterIndex, thermalCtrlIndex, fanSpeedInfo);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_FanSpeedInfo_Get error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_FanSpeed_Get(int adapterIndex, int thermalCtrlIndex,
-                ADLFanSpeedValue* fanSpeedValue) const
+void ATIADLHandle::Overdrive5_FanSpeed_Get(int adapterIndex, int thermalCtrlIndex, ADLFanSpeedValue* fanSpeedValue) const
 {
-    int error = pADL_Overdrive5_FanSpeed_Get(adapterIndex, thermalCtrlIndex,
-                    fanSpeedValue);
+    int error = pADL_Overdrive5_FanSpeed_Get(adapterIndex, thermalCtrlIndex, fanSpeedValue);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_FanSpeed_Get error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_ODParameters_Get(int adapterIndex,
-                ADLODParameters* odParameters) const
+void ATIADLHandle::Overdrive5_ODParameters_Get(int adapterIndex, ADLODParameters* odParameters) const
 {
     int error = pADL_Overdrive5_ODParameters_Get(adapterIndex, odParameters);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_ODParameters_Get error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_ODPerformanceLevels_Get(int adapterIndex, int idefault,
-                ADLODPerformanceLevels* odPerformanceLevels) const
+void ATIADLHandle::Overdrive5_ODPerformanceLevels_Get(int adapterIndex, int idefault, ADLODPerformanceLevels* odPerformanceLevels) const
 {
-    int error = pADL_Overdrive5_ODPerformanceLevels_Get(adapterIndex, idefault,
-                    odPerformanceLevels);
+    int error = pADL_Overdrive5_ODPerformanceLevels_Get(adapterIndex, idefault, odPerformanceLevels);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_ODPerformanceLevels_Get error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_FanSpeed_Set(int adapterIndex, int thermalCtrlIndex,
-                ADLFanSpeedValue* fanSpeedValue) const
+void ATIADLHandle::Overdrive5_FanSpeed_Set(int adapterIndex, int thermalCtrlIndex, ADLFanSpeedValue* fanSpeedValue) const
 {
-    int error = pADL_Overdrive5_FanSpeed_Set(adapterIndex, thermalCtrlIndex,
-                    fanSpeedValue);
+    int error = pADL_Overdrive5_FanSpeed_Set(adapterIndex, thermalCtrlIndex, fanSpeedValue);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_FanSpeed_Set error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_FanSpeedToDefault_Set(int adapterIndex,
-                int thermalCtrlIndex) const
+void ATIADLHandle::Overdrive5_FanSpeedToDefault_Set(int adapterIndex, int thermalCtrlIndex) const
 {
     int error = pADL_Overdrive5_FanSpeedToDefault_Set(adapterIndex, thermalCtrlIndex);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_FanSpeedToDefault_Set error");
+    }
 }
 
-void ATIADLHandle::Overdrive5_ODPerformanceLevels_Set(int adapterIndex,
-                ADLODPerformanceLevels* odPerformanceLevels) const
+void ATIADLHandle::Overdrive5_ODPerformanceLevels_Set(int adapterIndex, ADLODPerformanceLevels* odPerformanceLevels) const
 {
     int error = pADL_Overdrive5_ODPerformanceLevels_Set(adapterIndex, odPerformanceLevels);
+
     if (error != ADL_OK)
+    {
         throw Error(error, "ADL_Overdrive5_ODPerformanceLevels_Set error");
+    }
 }
 
 class ADLMainControl
 {
+
 private:
+
     const ATIADLHandle& handle;
     int fd;
     bool mainControlCreated;
     bool withX;
+
 public:
+
     explicit ADLMainControl(const ATIADLHandle& handle, int devId);
     ~ADLMainControl();
     
