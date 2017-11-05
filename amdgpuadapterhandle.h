0ba104e6845881111a4ecaaa0198dda34207422e
Willem van Ketwich <willvk@gmail.com>
2017-11-05 19:30:11 +1100

    includes

--- amdgpuadapterhandle.h
+++ amdgpuadapterhandle.h
@@ -1,6 +1,44 @@
 #ifndef AMDGPUADAPTERHANDLE_H
 #define AMDGPUADAPTERHANDLE_H
 
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
+#endif
+
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <dlfcn.h>
+#include <fstream>
+#include <algorithm>
+#include <cstdio>
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <memory>
+#include <cmath>
+#include <cstdarg>
+#include <stdint.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <CL/cl.h>
+
+extern "C" {
+#include <pci/pci.h>
+}
+
+#ifdef __linux__
+#define LINUX 1
+#endif
+#include "./dependencies/ADL_SDK_V10.2/include/adl_sdk.h"
+
+#include "error.h"
+#include "atiadlhandle.h"
+#include "adlmaincontrol.h"
+
 class AMDGPUAdapterHandle
 {
 
@@ -30,7 +68,7 @@ public:
     void setOverdriveCoreParam(int adapterIndex, unsigned int coreOD) const;
 
     void setOverdriveMemoryParam(int adapterIndex, unsigned int memoryOD) const;
-    
+
     void getPerformanceClocks(int adapterIndex, unsigned int& coreClock, unsigned int& memoryClock) const;
 };
 
