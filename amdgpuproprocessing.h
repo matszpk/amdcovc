87aab21094b3b8fac2231f5f07637b66e173cbd8
Willem van Ketwich <willvk@gmail.com>
2017-11-06 16:30:54 +1100

    includes

--- amdgpuproprocessing.h
+++ amdgpuproprocessing.h
@@ -1,6 +1,35 @@
 #ifndef AMDGPUPROPARAMETERS_H
 #define AMDGPUPROPARAMETERS_H
 
+#define _DEFAULT_SOURCE
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <dlfcn.h>
+#include <fstream>
+#include <algorithm>
+#include <cstdio>
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <memory>
+#include <cmath>
+#include <cstdarg>
+#include <stdint.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <CL/cl.h>
+extern "C" {
+#include <pci/pci.h>
+}
+
+#ifdef __linux__
+#define LINUX 1
+#endif
+
 class AmdGpuProProcessing
 {
 
