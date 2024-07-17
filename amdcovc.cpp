50afb3178de81468c637086e0ea34058fa5d8fd4
matszpk <matszpk@interia.pl>
2024-07-17 16:08:27 +0200

    Update version.

--- amdcovc.cpp
+++ amdcovc.cpp
@@ -59,7 +59,7 @@ extern "C" {
 #include <adl_sdk.h>
 #endif
 
-#define AMDCOVC_VERSION "0.4.1.1"
+#define AMDCOVC_VERSION "0.4.1.2"
 
 enum TermColor {
     BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
